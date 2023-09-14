3e077a30712c409bdd8767943d5e8dd0544b45a5
mathew <mathewlau0714@gmail.com>
2023-09-14 12:50:15 +0800

    let's begin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title> Hello tit </title>
+        <meta charset="utf-8"/>
+    </head>
+
+    <body>
+        <?php 
+            echo "home page";
+        ?>
+    </body>
+
+</html>
+
